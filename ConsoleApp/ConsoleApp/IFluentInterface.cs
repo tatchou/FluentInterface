60fcfbc8bd0fe930e99376b6ea2d78a76ad4b154
ayman.chafei@gmail.com <ayman.chafei@gmail.com>
2020-09-19 21:18:40 -0400

    Fluent interface implementation

---
+++ ConsoleApp/ConsoleApp/IFluentInterface.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp
+{
+    public interface IFluentInterface
+    {
+        IFluentInterface DoSomething();
+        IFluentInterface DoSomethingElse();
+        void ThisMethodIsNotFluent();
+    }
+
+    public class FluentImplementation : IFluentInterface
+    {
+        public IFluentInterface DoSomething()
+        {
+            Console.WriteLine("Do Something");
+            return this;
+        }
+
+        public IFluentInterface DoSomethingElse()
+        {
+            Console.WriteLine("Do Something else");
+            return this;
+        }
+
+        public void ThisMethodIsNotFluent()
+        {
+            Console.WriteLine("Break fluent Pattern");
+        }
+    }
+}
---
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Start Program");
+            IFluentInterface fluent = new FluentImplementation();
+
+            fluent.DoSomething()
+                  .DoSomethingElse()
+                  .ThisMethodIsNotFluent();
+
+
+            Console.WriteLine("End of Program");
+            Console.ReadLine();
+        }
+    }
+}
